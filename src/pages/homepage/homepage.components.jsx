604ee10e79b2c5dc1f2b683fed912d0d7fff3099
adnan ali <adnanali4131@gmail.com>
2021-12-08 15:35:20 +0500

    borken  home page into small component

---
+++ src/pages/homepage/homepage.components.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './homepage.style.scss'
+import Directory from '../../components/directory/directory.components'
+
+const HomePage = () => {
+  return <Directory />
+}
+
+export default HomePage
